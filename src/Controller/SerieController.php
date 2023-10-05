abc9b5ca5610c04dda6f81536d37a76104103866
Laurie Boissay <laurie.boissay@laposte.net>
2023-10-05 15:39:25 +0200

    Création du repository serie, ajout de méthodes dans la classe serie, mise à jour des pages liste et détails, ajout de toutes les images.

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
-//use http\Env\Request;
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,20 +18,32 @@ class SerieController extends AbstractController
     /**
      * @Route("", name="list")
      */
-    public function list(): Response
+    public function list(SerieRepository $serieRepository): Response
     {
-        // Aller chercher les series en BdD.
+        // Récupère un tableau de séries à partir du repository de séries.
+        $series = $serieRepository->findBy(
+            [], // Tableau de critères de recherche, vide ici pour obtenir toutes les séries.
+            ['popularity' => 'DESC', 'vote' => 'DESC'], // Spécifie les critères de tri.
+            30 // Limite le nombre de résultats à 30 séries.
+        // Offset 10 // Affiche les séries de 10 à 40.
+        );
+
+        // dd($series); // DEBUG
+
         return $this->render('serie/list.html.twig', [
+            "series" => $series
         ]);
     }
 
     /**
      * @Route("/details{id}", name="details")
      */
-    public function details(int $id): Response
+    public function details(int $id, SerieRepository $serieRepository): Response
     {
-        // Aller chercher la serie en BdD.
+        $serie = $serieRepository->find($id);
+
         return $this->render('serie/detail.html.twig', [
+            "serie" => $serie
         ]);
     }
 
@@ -38,12 +52,51 @@ class SerieController extends AbstractController
      */
     public function create(Request $request): Response
     {
-        //dump("test Dump");
-        //dd("yo");
-        //dd($request);
-        dump($request);
+        // dd($request); // DEBUG
+        dump($request);  // DEBUG
 
-        // Aller chercher la serie en BdD.
+        // Aller chercher la série en BdD.
+        return $this->render('serie/create.html.twig');
+    }
+
+    /**
+     * @Route("/demo", name="em-demo")
+     */
+    public function demo(EntityManagerInterface $entityManager): Response
+    {
+        // Crée une instance de l'entité Serie :
+        $serie = new Serie();
+
+        // Hydrate toutes les propriétés :
+        $serie->setName('exemple');
+        $serie->setBackdrop('exemple');
+        $serie->setPoster('exemple');
+        $serie->setDateCreated(new \DateTime());
+        $serie->setFirstAirDate(new \DateTime("- 1 year"));
+        $serie->setLastAirDate(new \DateTime("- 6 month"));
+        $serie->setGenres('exemple');
+        $serie->setOverview('exemple');
+        $serie->setPopularity(123.00);
+        $serie->setVote(8.2);
+        $serie->setStatus('Canceled');
+        $serie->setTmdbID(329432);
+
+        $entityManager->persist($serie); // Enregistre
+        $entityManager->flush(); // Confirme + envoie la requête INSERT.
+        // Les requêtes doctrine empêchent les injections SQL en BdD.
+
+        dump($serie); // DEBUG
+
+        $serie->setGenres('anotherExemple'); // Update
+        $entityManager->flush(); // Confirme + envoie de la requête UPDATE.
+
+        dump($serie); // DEBUG
+
+        $entityManager->remove($serie); // Delete
+        $entityManager->flush(); // Confirme + envoie de la requête DELETE.
+
+        dump($serie); // DEBUG
+
         return $this->render('serie/create.html.twig');
     }
 }
